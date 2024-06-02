7331cbf41dbbe76e58b1eaf93db8ea79aef1d29a
gajevski <mikolajxgajewski@gmail.com>
2024-06-02 21:41:19 +0200

    struct exercise

--- main.go
+++ main.go
@@ -2,19 +2,35 @@ package main
 
 import (
 	"fmt"
-	"net/http"
+	"math"
 )
 
-func main() {
-	startServer()
+const (
+	PI = 3.14
+)
+
+type Circle struct {
+	radius float64
 }
 
-func startServer() {
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello, you've requested: %s\n", r.URL.Path)
-		fmt.Println("Server running")
-		fmt.Println("Hello, you've requested:", r.URL.Path)
-	})
+func CircleFactory(radius float64) Circle {
+	return Circle{
+		radius: radius,
+	}
+}
 
-	http.ListenAndServe(":80", nil)
+func (circle Circle) calculateCircumference() {
+	circumference := 2 * PI * circle.radius
+	fmt.Println(circumference)
+}
+
+func (circle Circle) calculateArea() {
+	area := PI * math.Pow(circle.radius, 2)
+	fmt.Println(area)
+}
+
+func main() {
+	circle := CircleFactory(1.5)
+	circle.calculateCircumference()
+	circle.calculateArea()
 }
